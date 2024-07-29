c012836c5cb65b04657e68290a2dd0206f3648ca
María Inés Cardona López <ines7r@gmail.com>
2024-07-29 08:56:40 -0600

    CrearProducto

--- Laboratorio 1/Tienda.cs
+++ Laboratorio 1/Tienda.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Laboratorio_1
 {
     public class Tienda
     {
+        public string Producto { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+        public double Compra { get; set; }
 
+        public Tienda(string producto, double precio, int cantidad, double compra)
+        {
+            Producto = producto;
+            Precio = precio;
+            Cantidad = cantidad;
+            Compra = compra;
+        }
+
+        public void CrearProducto()
+        {
+            Console.WriteLine($"Ingrese el nombre del producto: {Producto}");
+            Console.WriteLine($"Ingrese el precio del: {Precio}");
+            Console.WriteLine($"Ingrese la cantidad del producto: {Cantidad}");
+
+        }
 
     }
 }
+
